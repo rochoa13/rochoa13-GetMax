1043cda89cdf9df1d82c6ee3625c0d459837fb17
rochoa13 <ochoarafael.jr@gmail.com>
2023-02-14 23:58:55 +0000

    Complete ArrayMax

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -7,6 +7,14 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
-    }
+       int arrayMax = arr[0]; //sets the first value of the array as max
+       
+       for(int i = 0; i < arr.length; i++){
+        if(arr[i] > arrayMax){ //if value of current array index is greater than the current MAX, then arrayMax is updated to the current array index value
+            arrayMax = arr[i];
+        }
+       }
+       
+       return arrayMax; //returns the array max value after finishing updates through the loop
+     }
 }
